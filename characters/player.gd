a4aa97365df99f5a4d9e1026e9a74d9e776bd868
Wata <wata_qsk@proton.me>
2026-04-17 20:23:54 -0300

    added walk animation

--- characters/player.gd
+++ characters/player.gd
@@ -7,6 +7,16 @@ extends CharacterBody3D
 @export_range(0, 360, 1) var walk_speed: float = 90.0
 @export_range(0, 360, 1) var run_speed: float = 90.0
 
+var state_machine: AnimationNodeStateMachinePlayback
+
+func _ready() -> void:
+	state_machine = $AnimationTree.get("parameters/Movement/playback") as AnimationNodeStateMachinePlayback	
+
+func _physics_process(delta: float) -> void:
+	handle_turn(delta)
+	handle_walk(delta)
+	move_and_slide()
+
 func handle_turn(delta: float) -> void:
 	var turn_dir = Input.get_axis("turn_left", "turn_right")
 	rotation_degrees.y -= turn_dir * turn_speed * delta
@@ -18,7 +28,9 @@ func handle_walk(delta: float) -> void:
 	velocity.x = walk_velocity.x
 	velocity.z = walk_velocity.z
 
-func _physics_process(delta: float) -> void:
-	handle_turn(delta)
-	handle_walk(delta)
-	move_and_slide()
+	if walk_velocity.length_squared() > 0:
+		$AnimationTree.set("parameters/Movement/conditions/is_walking", true)
+		$AnimationTree.set("parameters/Movement/conditions/idle", false)
+	else:
+		$AnimationTree.set("parameters/Movement/conditions/idle", true)
+		$AnimationTree.set("parameters/Movement/conditions/is_walking", false)
