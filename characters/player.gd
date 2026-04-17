36cd0330daecaae9f54e161ea8daf77d8494d58a
Wata <wata_qsk@proton.me>
2026-04-17 00:20:09 -0300

    preparing for the walk animation

--- characters/player.gd
+++ characters/player.gd
@@ -1,17 +1,20 @@
-class_name Player extends CharacterBody3D
+class_name Player
+extends CharacterBody3D
 
+@export_category("Player Stats")
 @export_group("Movement Settings")
-@export var turn_speed := 180.0
-@export var walk_speed := 80.0
-@export var run_speed := 280.0
+@export_range(0, 360, 1) var turn_speed: float = 90.0
+@export_range(0, 360, 1) var walk_speed: float = 90.0
+@export_range(0, 360, 1) var run_speed: float = 90.0
 
-func handle_turn(delta):
-	var turn_dir = Input.get_axis ("turn_left","turn_right")
+func handle_turn(delta: float) -> void:
+	var turn_dir = Input.get_axis("turn_left", "turn_right")
 	rotation_degrees.y -= turn_dir * turn_speed * delta
 
-func handle_walk(delta):
-	var input_dir = Input.get_axis("move_backward","move_forward")	
+func handle_walk(delta: float) -> void:
+	var input_dir = Input.get_axis("move_backward", "move_forward")
 	var walk_velocity = basis.z * input_dir * walk_speed * delta
+
 	velocity.x = walk_velocity.x
 	velocity.z = walk_velocity.z
 
