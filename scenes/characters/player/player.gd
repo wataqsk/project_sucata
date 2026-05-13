4a6c81d57b7d2a08766f14a44a4899fa291f0ac4
almeidarv <gabrieldealmeidarvieira.12@gmail.com>
2026-05-13 20:01:13 -0300

    Coyote time

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -16,6 +16,8 @@ extends CharacterBody3D
 
 @export_group("Jump")
 @export var jump_force := 12.0 
+## Configuração do CoyoteTime abaixo, 0.1 parece ser o melhor mesmo, 0.2 parece ser um poooouco alto demais, se quiser dar uma testada
+@export var coyote_time := 0.1  # Tempo em segundos que o jogador pode pular depois de sair do chão
 
 ## Determina quanto tempo leva pro Mesh do player girar na direção da câmera
 @export_group("Character Skin")
@@ -31,6 +33,11 @@ var _last_move_direction := Vector3.BACK
 ## Estado de pulo
 var _is_jumping := false
 
+## Timer interno pro CoyoteTime
+var _coyote_timer := 0.0
+var _was_on_floor := false
+
+## Usando a gravidade default do Godot (normalmente 9.8)
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 var state_machine: AnimationNodeStateMachinePlayback
@@ -56,6 +63,7 @@ func _physics_process(delta: float) -> void:
 	_update_camera(delta)
 	_update_movement(delta)
 	_update_gravity(delta)
+	_update_coyote_time(delta)
 	_update_jump()
 	_update_animation()
 	move_and_slide()
@@ -79,12 +87,32 @@ func _update_gravity(delta: float) -> void:
 		
 		# Se estiver caindo (não subindo), aplica gravidade extra
 		if velocity.y < 0:
-			velocity.y -= gravity * delta * 3.0  # 50% mais gravidade na descida
+			velocity.y -= gravity * delta * 3.0  
+
+# Função para gerenciar o coyote time
+func _update_coyote_time(delta: float) -> void:
+	# Se estava no chão e agora não está mais
+	if _was_on_floor and not is_on_floor():
+		_coyote_timer = coyote_time
+	
+	# Se está no chão, reseta o timer
+	if is_on_floor():
+		_coyote_timer = 0.0
+	
+	# Decrementa o timer
+	if _coyote_timer > 0.0:
+		_coyote_timer -= delta
+	
+	# Atualiza o estado anterior
+	_was_on_floor = is_on_floor()
 
 func _update_jump() -> void:
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	var can_jump := is_on_floor() or _coyote_timer > 0.0
+	
+	if Input.is_action_just_pressed("jump") and can_jump:
 		velocity.y = jump_force
 		_is_jumping = true
+		_coyote_timer = 0.0  # Reseta o timer ao pular
 
 func _check_landing() -> void:
 	if is_on_floor() and _is_jumping:
