0405c550f6b510659d01cd4cdcf9bbecfa87254c
almeidarv <gabrieldealmeidarvieira.12@gmail.com>
2026-05-13 19:45:46 -0300

    jump jump jump

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -5,7 +5,6 @@ extends CharacterBody3D
 @export_range(0.0, 1.0) var camera_sensitivity := 0.25
 
 ## Determina o limite da câmera vertical
-## Não tá mais hard-codado! Da pra alterar no inspector.
 @export_group("Camera Pitch")
 @export_range(-90.0, 0.0, 1.0, "suffix:°") var camera_pitch_min := -30.0
 @export_range(0.0, 90.0, 1.0, "suffix:°") var camera_pitch_max := 60.0
@@ -14,6 +13,9 @@ extends CharacterBody3D
 @export_group("Character Speed")
 @export var move_speed := 8.0
 @export var acceleration := 20.0
+
+@export_group("Jump")
+@export var jump_force := 12.0 
 
 ## Determina quanto tempo leva pro Mesh do player girar na direção da câmera
 @export_group("Character Skin")
@@ -24,10 +26,12 @@ extends CharacterBody3D
 @onready var _skin: Node3D = %Mannequin
 
 var _camera_input_direction := Vector2.ZERO
-## Isso daqui é importante.
-## Armazena na memória a ultima direção na qual o Mesh estava se movendo.
-## Existe para manter o Mesh virado quando o player largar o controle.
 var _last_move_direction := Vector3.BACK
+
+## Estado de pulo
+var _is_jumping := false
+
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 var state_machine: AnimationNodeStateMachinePlayback
 
@@ -51,13 +55,15 @@ func _unhandled_input(event: InputEvent) -> void:
 func _physics_process(delta: float) -> void:
 	_update_camera(delta)
 	_update_movement(delta)
+	_update_gravity(delta)
+	_update_jump()
 	_update_animation()
 	move_and_slide()
+	
+	_check_landing()
 
 func _update_camera(delta: float) -> void:
 	_camera_pivot.rotation.x += _camera_input_direction.y * delta
-	## Agora o código não ta mais hard-codado e converte o degrees para radians.
-	## (Exemplo: A gente pode colocar 180° graus no inpsector ao inves de [Insira número satânico])
 	_camera_pivot.rotation.x = clamp(
 		_camera_pivot.rotation.x,
 		deg_to_rad(camera_pitch_min),
@@ -66,14 +72,29 @@ func _update_camera(delta: float) -> void:
 	_camera_pivot.rotation.y -= _camera_input_direction.x * delta
 	_camera_input_direction = Vector2.ZERO
 
+func _update_gravity(delta: float) -> void:
+	if not is_on_floor():
+		# Gravidade normal
+		velocity.y -= gravity * delta
+		
+		# Se estiver caindo (não subindo), aplica gravidade extra
+		if velocity.y < 0:
+			velocity.y -= gravity * delta * 3.0  # 50% mais gravidade na descida
+
+func _update_jump() -> void:
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_force
+		_is_jumping = true
+
+func _check_landing() -> void:
+	if is_on_floor() and _is_jumping:
+		_is_jumping = false
+
 func _update_movement(delta: float) -> void:
 	var raw_input := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	## [Leia ali em baixo primeiro, ou não... ]
-	## Provavelmente teriamos que alterar esse cara aqui por conta do -Z. (Z Negativo)
-	## "_camera.global_basis.z" -> "-_camera.global_basis.z"
+
 	var forward := _camera.global_basis.z
 	var right := _camera.global_basis.x
-
 	var move_direction := (forward * raw_input.y + right * raw_input.x)
 	move_direction.y = 0.0
 
@@ -83,25 +104,13 @@ func _update_movement(delta: float) -> void:
 
 	velocity = velocity.move_toward(move_direction * move_speed, acceleration * delta)
 
-	## A matemática pro Mesh do player rodar ta invertida.
-	## Diferente de alguns programas, Godot usa -Z (Z Negativo) como Axis da frente.
-	## Ta invertido porque dai eu não preciso girar o Player Mesh em 180deg.
-	## Olha o Vector3.BACK, se fosse o Z positivo seria Vector3.FORWARD
-	## (E provavelmente teria de negativar o var forward. [Leia ali em cima])
 	var target_angle := Vector3.BACK.signed_angle_to(_last_move_direction, Vector3.UP)
-	## Essa parte é interessante, o lerp_angle interpola (se acostume com essa palavra)
-	## A rotação do mesh, fazendo que ela seja progressiva ao invés de imediata.
-	## Fazendo com que a animação percorra um caminho antes de virar-se.
-	## Basicamente, deixa a coisa mais fluida!
-	## Descomenta o código abaixo e comenta o larp_angle para você vê a diferença.
-	## _skin.rotation.y = target _angle
 	_skin.rotation.y = lerp_angle(_skin.rotation.y, target_angle, rotation_speed * delta)
 
-## Existe aqui o mesmo problema do Unity.
-## O personagem sempre passa pelo Idle quando você anda pro lado oposto.
-## Apesar de que aqui parece um pouco mais sútil, ainda preciso ver melhor.
 func _update_animation() -> void:
-	if velocity.length_squared() > 0.0:
+	if _is_jumping:
+		state_machine.travel("Jump")
+	elif velocity.length_squared() > 0.0:
 		state_machine.travel("Walk")
 	else:
 		state_machine.travel("Idle")
